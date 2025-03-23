e1bbf989969d16b833129ab581976cba05c18258
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-22 18:07:00 -0700

    add component and pages to the frontend

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+function HomePage() {
+    return (
+        <main>
+            <h2>Home Page</h2>
+        </main>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/RoadmapPage.jsx
@@ -0,0 +1,9 @@
+function RoadmapPage() {
+    return (
+        <div>
+            <h1>Roadmap</h1>
+        </div>
+    );
+};
+
+export default RoadmapPage;
